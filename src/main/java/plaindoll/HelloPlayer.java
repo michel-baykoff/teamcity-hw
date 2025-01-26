5eede9a3f16800c16b6834b3e4c044b0a5812b9e
Anton <ant.band@mail.ru>
2025-01-27 00:28:42 +0300

    test 89

--- src/main/java/plaindoll/HelloPlayer.java
+++ src/main/java/plaindoll/HelloPlayer.java
@@ -5,6 +5,6 @@ public class HelloPlayer{
 		Welcomer welcomer = new Welcomer();
 		System.out.println(welcomer.sayWelcome());
 		System.out.println(welcomer.sayFarewell());
-		System.out.println("test");
+		System.out.println("test 89");
 	}
 }
