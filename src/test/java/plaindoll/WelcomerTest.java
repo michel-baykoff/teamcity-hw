e030bfda1a55202077778446454a0a40483bbd31
alextee-dev <sicksong@gmail.com>
2025-03-08 15:37:36 +0700

    fix1

--- src/test/java/plaindoll/WelcomerTest.java
+++ src/test/java/plaindoll/WelcomerTest.java
@@ -32,6 +32,6 @@ public class WelcomerTest {
 	}
 	@Test
 	public void sayHunterReplica(){
-		assertThat(welcomer.saySome(), containsString("hunter"));
+		assertThat(welcomer.sayHunterReplica(), containsString("hunter"));
 	}
 }
