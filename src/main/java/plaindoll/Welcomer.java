7cb1763142a83f2543c11c25ac23583ceb445e16
alextee-dev <sicksong@gmail.com>
2025-03-08 15:35:03 +0700

    add replica

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -1,6 +1,13 @@
 package plaindoll;
 
+import java.util.Random;
+
 public class Welcomer{
+	private Random random;
+
+	public Welcomer() {
+        random = new Random();
+    }
 	public String sayWelcome() {
 		return "Welcome home, good hunter. What is it your desire?";
 	}
@@ -12,5 +19,15 @@ public class Welcomer{
 	}
 	public String saySome(){
 		return "something in the way";
+	}
+
+	public String sayHunterReplica() {
+	String[] replicas = {
+		"Welcome home, good hunter. What is it your desire?",
+		"Farewell, good hunter. May you find your worth in waking world.",
+	};
+
+	int index = random.nextInt(replicas.length);
+	return replicas[index];
 	}
 }
