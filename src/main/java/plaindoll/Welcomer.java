e9d0280e15e67bc771c590ea81db876ee9480628
Hoaxlt <vboxuser@ubuntuu.myguest.virtualbox.org>
2025-04-20 09:01:52 +0300

    hunter-fix-01

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -17,4 +17,4 @@ public class Welcomer{
 		return "Hello, hunter!";
 	}
 }
-#
+
--- src/test/java/plaindoll/WelcomerTest.java
+++ src/test/java/plaindoll/WelcomerTest.java
@@ -31,7 +31,7 @@ public class WelcomerTest {
 		assertThat(welcomer.saySome(), containsString("something"));
 	}
 	@Test
-	public void welcomerSaysHunter(){
+	public void welcomerSaysGreetings(){
 		assertThat(welcomer.sayHunter(), containsString("hunter"));
 		assertThat(welcomer.sayHunter(), containsString("Hello"));
 	}
