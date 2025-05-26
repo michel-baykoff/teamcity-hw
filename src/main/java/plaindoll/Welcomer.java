d8d0d3b7c6234b647bd54b21c3531c86fc1c594d
Myash-New <52988552+Myash-New@users.noreply.github.com>
2025-05-27 01:09:23 +0300

    Update Welcomer.java

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -1,8 +1,8 @@
 package plaindoll;
 
 public class Welcomer{
-	public String sayWelcome() {
-		return "Welcome home, good hunter. What is it your desire?";
+	public String sayHello() {
+		return "Get over here, hunter";
 	}
 	public String sayFarewell() {
 		return "Farewell, good hunter. May you find your worth in waking world.";
