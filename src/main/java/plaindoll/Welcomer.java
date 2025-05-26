dfc92a4c8f18e65b3609d75ce34aa99ce2c7cced
Myash-New <52988552+Myash-New@users.noreply.github.com>
2025-05-27 01:31:50 +0300

    Update Welcomer.java

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -4,6 +4,10 @@ public class Welcomer{
 	public String sayHello() {
 		return "Get over here, hunter";
 	}
+	public String sayWelcome() {
+		return "Welcome home, good hunter. What is it your desire?";
+	}
+	}
 	public String sayFarewell() {
 		return "Farewell, good hunter. May you find your worth in waking world.";
 	}
